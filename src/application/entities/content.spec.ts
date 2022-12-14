f53b73d220050896bdc1516da1b911163e355bee
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-12-14 17:02:30 -0300

    fix: fixed Replace import at notification file

--- src/application/entities/content.spec.ts
+++ src/application/entities/content.spec.ts
@@ -1,6 +1,6 @@
 import { Content } from './content';
 
-describe('Notification content', () => {
+describe('Notification Content', () => {
   it('should be able to create a notification content', () => {
     const content = new Content('Você recebeu uma solicitação de amizade');
 
