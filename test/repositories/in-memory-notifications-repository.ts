ca1134d0d580c93b75257834d13f50d24b7cb03c
Deywerson Pereira <deywerson.pereira@gmail.com>
2022-12-15 10:43:10 -0300

    create: created cancel-notification use case

--- test/repositories/in-memory-notifications-repository.ts
+++ test/repositories/in-memory-notifications-repository.ts
@@ -5,7 +5,15 @@ export class InMemoryNotificationsRepository
   implements NotificationsRepository {
   public notifications: Notification[] = [];
 
+  async findById(notificationId: string): Promise<Notification | null> {
+    throw new Error('Method not implemented.');
+  }
+
   async create(notification: Notification) {
     this.notifications.push(notification);
+  }
+
+  async save(notification: Notification): Promise<void> {
+    throw new Error('Method not implemented.');
   }
 }
